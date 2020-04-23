61f90488dcbe579ea17ca8de935ac03f50685866
Guillaume Marcellin <marcellinguillaume@hotmail.fr>
2020-04-23 17:42:01 +0200

    new routes + rename one-product.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const Sneaker = require("../models/Sneaker")
 
 router.get("/", (req, res) => {
-  res.render("index.hbs"); 
+  res.render("index.hbs");
 });
 
 router.get("/sneakers/:cat", (req, res) => {
-  res.send("bar");
+
+  if (req.params.cat === "collection") {
+    Sneaker.find().then(dbResult => {
+      res.render("products.hbs", {
+        sneakers: dbResult
+      })
+    }).catch(dbErr => console.log(dbErr))
+  } else {
+    Sneaker.find({
+      category: req.params.cat
+    }).then(dbResult => {
+      res.render("products.hbs", {
+        sneakers: dbResult
+      })
+    }).catch(dbErr => console.log(dbErr))
+  
+  }
 });
 
 router.get("/one-product/:id", (req, res) => {
-  res.render("one-product.hbs");
+  Sneaker.findById(req.params.id).then(dbResult=>{
+    res.render("one-product.hbs", {
+      sneaker:dbResult
+    });
+  }).catch(dbErr => console.log(dbErr))
+  
 });
 
 
